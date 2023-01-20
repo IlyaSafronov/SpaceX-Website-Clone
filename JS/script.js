a5a3107fa0fe2abc354426316fb66a19ce964917
Ilya Safronov <IlyaSafronov18@gmail.com>
2023-01-21 01:21:07 +0300

    start count

--- JS/script.js
+++ JS/script.js
@@ -2,8 +2,10 @@
 const btn = document.getElementById('menu-btn');
 const overlay = document.getElementById('overlay');
 const menu = document.getElementById('mobile-menu');
+const counter = document.querySelectorAll('.counter');
 
 btn.addEventListener('click', navToggle);
+document.addEventListener('scroll', scrollPage);
 
 function navToggle() {
   btn.classList.toggle('open');
@@ -11,3 +13,44 @@ function navToggle() {
   document.body.classList.toggle('stop-scrolling');
   menu.classList.toggle('show-menu');
 }
+
+function scrollPage() {
+  const scrollPos = window.scrollY;
+  if(scrollPos > 100) {
+    countUp();
+  }else if(scrollPos < 100) {
+    reset();
+  }
+}
+
+function countUp() {
+  counter.forEach((counter) => {
+    counter.innerText = '0';
+
+    const updateCounter = () => {
+      // get count target
+      const target = +counter.getAttribute('data-target');
+      // get current caunter value
+      const c = +counter.innerText;
+      // create an increment
+      const increment = target / 100;
+      // if counter is less than target, add increment
+      if (c < target) {
+        // round up and set counter value
+        counter.innerText = `${Math.ceil(c + increment)}`;
+
+        setTimeout(updateCounter, 75);
+      } else {
+        counter.innerText = target;
+      }
+
+    };
+    updateCounter();
+  });
+}
+
+function reset() {
+  counters.forEach((counter) => (counter.innerHTML = '0'));
+}
+
+// countUp();
